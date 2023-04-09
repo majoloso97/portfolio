cf0bc7c8d8edc2b46c7f1453484095c5482acecf
majoloso97 <majoloso97@gmail.com>
2023-04-09 05:59:44 -0600

    :construction: Add dark theme to footer and home

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <footer>
+    <footer className="bg-white dark:bg-slate-800 text-white h-1/4">
       <p>&copy; 2023 My Portfolio</p>
     </footer>
   );
--- pages/index.tsx
+++ pages/index.tsx
@@ -6,9 +6,11 @@ const Home = () => {
   return (
     <div>
       <Header />
-      <main>
-        <h1 className='text-sky-400'>Welcome to My Portfolio</h1>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam auctor leo sit amet quam aliquet, vel accumsan nunc efficitur. Aenean vel fermentum arcu, a luctus elit. Duis vel nulla nisi. </p>
+      <main className='bg-white dark:bg-slate-600 h-screen'>
+        <h1 className='text-sky-900 dark:text-sky-500'>Welcome to My Portfolio</h1>
+        <p className='text-black dark:text-sky-100'>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam auctor leo sit amet quam aliquet, vel accumsan nunc efficitur. Aenean vel fermentum arcu, a luctus elit. Duis vel nulla nisi.
+          </p>
       </main>
       <Footer />
     </div>
